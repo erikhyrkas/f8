f575fc82abd6c61989fd7a97987cbfd33b43549b
Erik Hyrkas <erik.hyrkas@gmail.com>
2022-11-10 06:37:06 -0600

    Create unit_test.hpp

---
+++ src/unit_test.hpp
@@ -0,0 +1,35 @@
+//
+// Created by Erik Hyrkas on 10/25/2022.
+//
+
+#ifndef MICROML_UNIT_TEST_HPP
+#define MICROML_UNIT_TEST_HPP
+
+#define ASSERT_TRUE(arg) \
+            if(!(arg)) { \
+                std::cout << "Test failed at " \
+                          << __FILE__ << ", " << __LINE__ << ", " << __func__ << ": " \
+                          << #arg \
+                          << std::endl; \
+               throw std::exception("Test failed."); \
+            } \
+            std::cout << "Test passed at " \
+                          << __FILE__ << ", " << __LINE__ << ", " << __func__ << ": " \
+                          << #arg \
+                          << std::endl
+
+#define ASSERT_FALSE(arg) \
+            if((arg)) { \
+                std::cout << "Test failed at " \
+                          << __FILE__ << ", " << __LINE__ << ", " << __func__ << ": " \
+                          << #arg \
+                          << std::endl; \
+               throw std::exception("Test failed."); \
+            }             \
+            std::cout << "Test passed at " \
+                          << __FILE__ << ", " << __LINE__ << ", " << __func__ << ": " \
+                          << #arg \
+                          << std::endl
+
+
+#endif //MICROML_UNIT_TEST_HPP
